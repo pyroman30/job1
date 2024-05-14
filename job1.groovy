3119eeec309f2b99f6f833e124d0919c91c97c88
Roman Karachenkov <RKarachenkov@alfabank.ru>
2024-05-14 18:52:35 +0300

    hello world
    as
    
    123

--- job1.groovy
+++ job1.groovy
@@ -4,8 +4,9 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                sh "echo 'hello'"
+                sh "echo 'hello world'"
             }
         }
     }
-}
+}234
+dsaf
