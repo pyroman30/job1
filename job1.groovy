8cf950e3830a4bd16f0b10995af50ba551b864b2
Roman Karachenkov <RKarachenkov@alfabank.ru>
2024-05-15 16:44:37 +0300

    1

--- job1.groovy
+++ job1.groovy
@@ -13,10 +13,25 @@ pipeline {
                     def latestReleaseBranch = sh(script: "git ls-remote --heads origin | grep 'refs/heads/release/' | awk -F'/' '{print \$3 \"/\" \$4 \$5}' | sort -V | tail -n1", returnStdout: true).trim()
                     echo "$latestReleaseBranch"
 
-              
+                    // Установка найденной ветки в параметр BRANCH
+                    params.BRANCH = latestReleaseBranch
                 }
             }
         }
 
+        stage('Execute Command in Latest Release Branch') {
+            when {
+                expression { params.BRANCH != '' }
+            }
+            steps {
+                script {
+                    // Переключение на последнюю релизную ветку
+                    checkout([$class: 'GitSCM', branches: [[name: params.BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/pyroman30/job1']]])
+
+                    // Выполнение команды в последней релизной ветке
+                    sh "echo 'hello'"
+                }
+            }
+        }
     }
 }
