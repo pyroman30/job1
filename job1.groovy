99cbd9dcb759f9d021f2f1b6a2dee8f84d5b276d
Roman Karachenkov <RKarachenkov@alfabank.ru>
2024-05-15 16:58:03 +0300

    1

--- job1.groovy
+++ job1.groovy
@@ -2,39 +2,30 @@ pipeline {
     agent any
 
     parameters {
-        string(name: 'BRANCH', defaultValue: '', description: 'Ветка для развертывания')
+        string(name: 'BRANCH', defaultValue: '', description: 'Branch to deploy from')
     }
 
     stages {
-        stage('Получение последней релизной ветки') {
+        stage('Get Latest Release Branch') {
             steps {
                 script {
-                    // Получение списка всех веток из репозитория
-                    def gitRemoteOutput = sh(script: "git ls-remote --heads origin", returnStdout: true).trim()
-                    echo "Результат выполнения команды git ls-remote --heads origin:\n${gitRemoteOutput}"
-
                     // Получение последней релизной ветки
-                    def latestReleaseBranch = sh(script: "git ls-remote --heads origin | grep 'refs/heads/release/' | awk -F'/' '{print \$3 \"/\" \$4 \$5}' | sort -V | tail -n1", returnStdout: true).trim()
+                    def latestReleaseBranch = sh(script: "git ls-remote --heads origin | grep 'refs/heads/release/' | awk -F'/' '{print $3 \"/\" $4 $5}' | sort -V | tail -n1", returnStdout: true).trim()
                     echo "Последняя релизная ветка: ${latestReleaseBranch}"
 
-                    // Установка найденной ветки в параметр BRANCH
-                    params.BRANCH = "$latestReleaseBranch"
+                    // Установка найденной ветки в переменную окружения BRANCH
+                    env.BRANCH = latestReleaseBranch
                 }
             }
         }
 
-        stage('Выполнение команды в последней релизной ветке') {
+        stage('Execute Command in Latest Release Branch') {
             when {
-                expression { params.BRANCH != '' && params.BRANCH != null }
+                expression { env.BRANCH != '' }
             }
             steps {
-                script {
-                    // Переключение на последнюю релизную ветку
-                    checkout([$class: 'GitSCM', branches: [[name: params.BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/pyroman30/job1']]])
-
-                    // Выполнение команды в последней релизной ветке
-                    sh "echo 'hello'"
-                }
+                echo "Выполнение команды в последней релизной ветке: ${env.BRANCH}"
+                // Добавьте здесь команды для выполнения в последней релизной ветке
             }
         }
     }
