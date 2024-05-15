b4b1b81377fa9b7bec91cf106b38bdb0b125be62
Roman Karachenkov <RKarachenkov@alfabank.ru>
2024-05-15 15:50:02 +0300

    1

--- job1.groovy
+++ job1.groovy
@@ -1,30 +1,12 @@
 pipeline {
     agent any
 
-    parameters {
-        string(name: 'BRANCH', defaultValue: '', description: 'Branch to deploy from')
-    }
-
     stages {
-        stage('Get Latest Release Branch') {
+        stage('Hello') {
             steps {
-                script {
-                    // Получение последней релизной ветки
-                    def latestReleaseBranch = sh(script: "git ls-remote --heads origin | grep 'refs/heads/release/' | cut -d'/' -f3 | sort -r | head -n1", returnStdout: true).trim()
-
-                    // Установка найденной ветки в параметр BRANCH
-                    params.BRANCH = latestReleaseBranch
-                }
-            }
-        }
-
-        stage('Deploy') {
-            when {
-                expression { params.BRANCH != '' }
-            }
-            steps {
-                echo "Deploying from branch: ${params.BRANCH}"
-                // Здесь вы можете добавить команды для деплоя из найденной релизной ветки
+                // Проверка кода из репозитория Git
+                checkout scm
+                sh "echo 'hello'"
             }
         }
     }
