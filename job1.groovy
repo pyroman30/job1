080a730fca87c37cac90aa719ef393aa865ad857
Roman Karachenkov <RKarachenkov@alfabank.ru>
2024-06-07 13:35:29 +0300

    test

--- job1.groovy
+++ job1.groovy
@@ -1,32 +1,30 @@
 pipeline {
     agent any
-
-    parameters {
-        string(name: 'BRANCH', defaultValue: '', description: 'Branch to deploy from')
-    }
-
+    
     stages {
-        stage('Get Latest Release Branch') {
+        stage('Run Python Script') {
             steps {
                 script {
-                    // Получение последней релизной ветки
-                    def latestReleaseBranch = sh(script: "git ls-remote --heads origin | grep 'refs/heads/release/' | awk -F'/' '{print $3 \"/\" $4 $5}' | sort -V | tail -n1", returnStdout: true).trim()
-                    echo "Последняя релизная ветка: ${latestReleaseBranch}"
-
-                    // Установка найденной ветки в переменную окружения BRANCH
-                    env.BRANCH = latestReleaseBranch
+                    // Запускаем скрипт Python
+                    def result = sh(script: 'python script.py', returnStatus: true)
+                    if (result != 0) {
+                        echo "Скрипт завершился с ошибкой (код возврата ${result})"
+                    }
                 }
             }
         }
-
-        stage('Execute Command in Latest Release Branch') {
-            when {
-                expression { env.BRANCH != '' }
-            }
-            steps {
-                echo "Выполнение команды в последней релизной ветке: ${env.BRANCH}"
-                // Добавьте здесь команды для выполнения в последней релизной ветке
-            }
+    }
+    
+    post {
+        always {
+            echo 'Этот шаг выполнится всегда'
+            // Отправляем POST-запрос
+            httpRequest(
+                url: 'http://example.com/api',
+                httpMode: 'POST',
+                requestBody: '{"key": "value"}',
+                contentType: 'APPLICATION_JSON'
+            )
         }
     }
 }
